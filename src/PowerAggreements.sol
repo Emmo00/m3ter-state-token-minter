5a4fc265678a99bbad1094f797fbde2cf750346b
Emmo00 <nwaforemmanuel005@gmail.com>
2025-08-10 16:38:02 +0100

    feat: Refactor PowerAggreements contract to remove ERC721 and AccessControl dependencies, and implement deposit and withdrawal functions for m3ter NFTs

--- src/PowerAggreements.sol
+++ src/PowerAggreements.sol
@@ -1,40 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract PowerAggreements is ERC721, AccessControl {
-    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-
-    constructor() ERC721("Power Aggrements", "PAs") {
-        _grantRole(ADMIN_ROLE, msg.sender);
-        _grantRole(MINTER_ROLE, msg.sender);
-    }
+contract PowerAggreements {
+    address public immutable m3terNFT;
 
     // mapping for m3ter token ID to Power aggrement token id check
     mapping(uint256 => bool) public m3terToPowerAgreement;
+    // m3ter id to owner
+    mapping(uint256 => address) public m3terToOwner;
 
-    function createPowerAgreement(address to, uint256 m3terTokenId) external onlyRole(MINTER_ROLE) {
-        require(m3terToPowerAgreement[m3terTokenId] == false, "PowerAggreements: Agreement already exists");
-
-        // Mint the Power Agreement token
-        _mint(to, m3terTokenId);
-
-        // Map the m3ter token ID to the Power Agreement token ID
-        m3terToPowerAgreement[m3terTokenId] = true;
+    constructor(address _m3terNFT) {
+        m3terNFT = _m3terNFT;
     }
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    function depositM3terNFT(uint256 tokenId) external {
+        require(IERC721(m3terNFT).ownerOf(tokenId) != address(0), "Invalid NFT");
+        require(m3terToPowerAgreement[tokenId] == false, "Already deposited");
+
+        _depositNFT(tokenId, msg.sender);
     }
 
-    function grantMinter(address account) external onlyRole(ADMIN_ROLE) {
-        grantRole(MINTER_ROLE, account);
+    function ownerOf(uint256 tokenId) external view returns (address) {
+        require(m3terToPowerAgreement[tokenId] == true, "No agreement found");
+        return m3terToOwner[tokenId];
     }
 
-    function revokeMinter(address account) external onlyRole(ADMIN_ROLE) {
-        revokeRole(MINTER_ROLE, account);
+    function transferAggreement(uint256 tokenId, address to) external {
+        require(m3terToPowerAgreement[tokenId] == true, "No agreement found");
+        require(m3terToOwner[tokenId] == msg.sender, "Not the owner");
+
+        m3terToOwner[tokenId] = to;
+        IERC721(m3terNFT).safeTransferFrom(address(this), to, tokenId);
+    }
+
+    function withdrawNFT(uint256 tokenId, address to) external {
+        require(m3terToPowerAgreement[tokenId] == true, "No agreement found");
+        require(m3terToOwner[tokenId] == msg.sender, "Not the owner");
+
+        m3terToPowerAgreement[tokenId] = false;
+        m3terToOwner[tokenId] = address(0);
+        IERC721(m3terNFT).safeTransferFrom(address(this), to, tokenId);
+    }
+
+    function onERC721Received(address operator, address, uint256 tokenId, bytes calldata) external returns (bytes4) {
+        require(msg.sender == m3terNFT, "Invalid NFT contract");
+
+        _depositNFT(tokenId, operator);
+
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function _depositNFT(uint256 tokenId, address from) internal {
+        m3terToPowerAgreement[tokenId] = true;
+        m3terToOwner[tokenId] = from;
+        
+        IERC721(m3terNFT).safeTransferFrom(from, address(this), tokenId);
     }
 }
